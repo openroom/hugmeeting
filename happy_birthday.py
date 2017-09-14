fef9fcaec2de769afa29c52fb3270fb9c5fb61cd
Kushal Hada <kushaldeveloper@gmail.com>
2017-09-14 07:57:52 -0400

    add auto pep8 add auto pep8 add auto pep8 add auto pep8

--- happy_birthday.py
+++ happy_birthday.py
@@ -1,9 +1,10 @@
-# filename: happy_birthday.py
-"""A basic (single function) API written using hug"""
+"""First hug API (local and HTTP access)"""
 import hug
 
 
-@hug.get('/happy_birthday')
-def happy_birthday(name, age:hug.types.number=1):
+@hug.get(examples='name=Timothy&age=26')
+@hug.local()
+def happy_birthday(name: hug.types.text, age: hug.types.number, hug_timer=3):
     """Says happy birthday to a user"""
-    return "Happy {age} Birthday {name}!".format(**locals())
+    return {'message': 'Happy {0} Birthday {1}!'.format(age, name),
+            'took': float(hug_timer)}
